001fe4a6f53a6cdc13fda7b8156daa22ee17b970
pandyans <54373028+pandyans@users.noreply.github.com>
2019-08-21 14:10:27 -0400

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,25 @@
+/*************************************
+** Hello Java
+*************************************
+** Print Hello Java
+**
+** Nupur Pandya
+** 08/21/19
+** CMSC 255 Section 2
+*************************************/
+
+public class Hello {
+	public static void printHeading () {
+		System.out.println("Nupur");
+		System.out.println("Hello Java");
+		System.out.println("Intro to Java");
+		System.out.println("August 21");
+		System.out.println("CMSC 255 Section 2");
+		System.out.println();
+	}
+	public static void main(String [] args) {
+		printHeading();
+		System.out.println("Hello");
+	}
+}
+	
